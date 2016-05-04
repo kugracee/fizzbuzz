aa637686381eb524eb30f85477b07afdc05ae1da
Grace Ku <grace.l.ku@gmail.com>
2016-05-04 13:22:09 -0400

    final

--- js/script.js
+++ js/script.js
@@ -4,18 +4,22 @@ $(document).ready(function() {
 for (var i=1; i<=100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
     	console.log ("FizzBuzz");
+    $("body").append("Fizzbuzz<br />");
     }
 
 	else if (i % 3 === 0) {
 		console.log("Fizz");
+	$("body").append("Fizz<br />");
 	}
 
 	else if (i % 5 === 0) {
 		console.log ("Buzz");
+	$("body").append("Buzz<br />")
 	}
 
 	else { 
 		console.log(i);
+	$("body").append(i + "<br />");
 	}
 }
 });
